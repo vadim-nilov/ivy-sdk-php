0095e3894f383a89644b146e6464cb82d0f43df2
Vadim <vadim.nilov@discoeat.com>
2023-02-16 16:57:52 +0100

    - add email description to refund request

--- src/Resources/Merchant/RefundRequest.php
+++ src/Resources/Merchant/RefundRequest.php
@@ -9,6 +9,7 @@ use Ivy\Resources\ApiResource;
  * @property string $referenceId
  * @property float $amount
  * @property float $description
+ * @property string $email
  */
 final class RefundRequest extends ApiResource
 {
